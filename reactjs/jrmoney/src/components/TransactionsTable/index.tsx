94737aa9f202f31ccb49ae9c2b19178f95c1c3d7
Josias Roberto <josiasroberto1@gmail.com>
2023-02-10 17:47:46 -0300

    Insert and List API transactions, format values

--- reactjs/jrmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/jrmoney/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+
+
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
   useEffect(() => {
     api.get('transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -21,22 +34,25 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>10/02/2023</td>
-          </tr>
+          {transactions.map(transaction => (
+            < tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
 
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">- R$1.100</td>
-            <td>Casa</td>
-            <td>08/02/2023</td>
-          </tr>
+              </td>
+            </tr>
 
+          ))}
         </tbody>
       </table>
-    </Container>
+    </Container >
   )
 }

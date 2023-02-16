9480a2ce42ee576b23a4f641f6ff573c04a53d8b
Josias Roberto <josiasroberto1@gmail.com>
2023-02-16 11:46:18 -0300

    Create Header component

--- reactjs/Chapter III/ignews/src/pages/index.tsx
+++ reactjs/Chapter III/ignews/src/pages/index.tsx
@@ -1,11 +1,5 @@
 import Head from 'next/head'
 
-// import styles from '../styles/global.module.scss'
-
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>

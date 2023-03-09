fac5220f3c68f8893c87ecae3bf6cf399e372eb0
Josias Roberto <josiasroberto1@gmail.com>
2023-03-09 13:52:31 -0300

    Finish Header component

--- reactjs/Chapter IV/dashgo/src/components/Header.tsx
+++ reactjs/Chapter IV/dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -46,6 +46,36 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Elvis Presley</Text>
+            <Text color="gray.300" fontSize="small">
+              elvisnaomorreu@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Elvis Presley" src="http://lorempixel.com.br/300/300/" />
+
+        </Flex>
       </Flex>
 
     </Flex>
